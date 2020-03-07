e95df95e428ac9021f4d1573e33eb9e8ac070ea9
Raiyan Zarif <razash139@gmail.com>
2020-03-07 17:17:52 +0200

    About page done

--- public/js/accTab.js
+++ public/js/accTab.js
@@ -2,7 +2,21 @@ const btns = document.querySelectorAll(".faq-accordion")
 
 btns.forEach(btn => {
     btn.addEventListener("click", () => {
+        btn.classList.toggle("active")
         const content = btn.nextElementSibling;
-        content.classList.toggle("active")
+        if (content.style.maxHeight) {
+            content.style.maxHeight = null;
+          } else {
+            content.style.maxHeight = content.scrollHeight + "px";
+          } 
     })
 })
+
+const changeIcon = (id) => {
+    const element = document.getElementById(id);
+    if (element.className == "fas fa-chevron-down fa-sm") {
+        element.className = "fas fa-chevron-up fa-sm"
+    } else {
+        element.className = "fas fa-chevron-down fa-sm"
+    }
+}
